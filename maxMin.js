1bbbb7faec0f2f4bf662ff7bd5c769dc919f1038
Shravan Manjunath <84725700+Manjunath599@users.noreply.github.com>
2022-06-26 10:28:00 +0530

    Add files via upload

---
+++ maxMin.js
@@ -0,0 +1,9 @@
+//A function which returns minimum and maximum values in an array
+function maxMin(arr){
+
+	return [Math.min(...arr) , Math.max(...arr)];
+  }
+  
+  console.log(maxMin([2,3,5,7,8,9]));
+
+
